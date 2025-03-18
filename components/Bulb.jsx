2f140c6c0d98a588acb2306981413c321b5215b1
Salman Sajid <salmansajidsattar@gmail.com>
2025-03-19 02:33:51 +0500

    update pakage.json file11

--- components/Bulb.jsx
+++ components/Bulb.jsx
@@ -4,7 +4,7 @@ const Bulb = () => {
   return (
     <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px] select-none pointer-events-none">
       <Image
-        href="bulb.png"
+        href="https://github.com/salmansajidsattar/Salman-Sajid-Portfolio/blob/main/public/bulb.png"
         alt="bulb"
         width={200}
         height={200}
