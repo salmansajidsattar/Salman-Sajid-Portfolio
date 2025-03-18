bdc005bb29b6f13ab33ead4686cc92a2b133cccc
Salman Sajid <salmansajidsattar@gmail.com>
2025-03-19 02:15:42 +0500

    update pakage.json file8

--- components/Bulb.jsx
+++ components/Bulb.jsx
@@ -4,7 +4,7 @@ const Bulb = () => {
   return (
     <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px] select-none pointer-events-none">
       <Image
-        src="/bulb.png"
+        src="bulb.png"
         alt="bulb"
         width={200}
         height={200}
